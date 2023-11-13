66a39153c6cad279038dbc5181dfd732a2440a50
Matthew Attree <u20431644@tuks.co.za>
2023-11-13 20:02:53 +0200

    Added comments and different charts

--- javascript/script.js
+++ javascript/script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Function to convert CSV to array of objects
+    // Function to convert CSV to an array of objects
     function csvToArray(csv) {
         var rows = csv.trim().split('\n');
         var headers = rows[0].split(',');
@@ -50,8 +50,8 @@ document.addEventListener('DOMContentLoaded', function () {
         return groupedData;
     }
 
-    // Function to calculate ave for a specific column
-    function calculateave(dataArray, column) {
+    // Function to calculate average for a specific column
+    function calculateAverage(dataArray, column) {
         var sum = dataArray.reduce(function (acc, row) {
             return acc + parseFloat(row[column]);
         }, 0);
@@ -59,18 +59,19 @@ document.addEventListener('DOMContentLoaded', function () {
         return sum / dataArray.length;
     }
 
-    // Fetch CSV data and calculate ave for workplaces_percent_change_from_baseline in all provinces
+    // Fetch CSV data and calculate average for workplaces_percent_change_from_baseline in all provinces
     fetchCsvData()
         .then(csvData => {
-            // Convert CSV to array of objects
+            // Convert CSV to an array of objects
             var dataArray = csvToArray(csvData);
 
             // Group data by month and province
             var groupedData = groupDataByMonthAndProvince(dataArray);
 
-            // Calculate ave for workplaces_percent_change_from_baseline in all provinces
+            // Calculate average for workplaces_percent_change_from_baseline in all provinces
             var ProvAve = {};
             var ProvAve_grocery = {};
+            var ProvAve_retail = {}; // Added for retail_and_recreation_percent_change_from_baseline
 
             for (var key in groupedData) {
                 if (groupedData.hasOwnProperty(key)) {
@@ -90,49 +91,110 @@ document.addEventListener('DOMContentLoaded', function () {
                         };
                     }
 
-                    ProvAve[provinceCode].sum += calculateave(groupedData[key], 'workplaces_percent_change_from_baseline');
+                    if (!ProvAve_retail[provinceCode]) {
+                        ProvAve_retail[provinceCode] = {
+                            sum: 0,
+                            count: 0,
+                        };
+                    }
+
+                    ProvAve[provinceCode].sum += calculateAverage(groupedData[key], 'workplaces_percent_change_from_baseline'); 
+                    //for each province code check the property and then push to the array for each column type
                     ProvAve[provinceCode].count += 1;
 
-                    ProvAve_grocery[provinceCode].sum += calculateave(groupedData[key], 'grocery_and_pharmacy_percent_change_from_baseline');
+                    ProvAve_grocery[provinceCode].sum += calculateAverage(groupedData[key], 'grocery_and_pharmacy_percent_change_from_baseline');
                     ProvAve_grocery[provinceCode].count += 1;
+
+                    ProvAve_retail[provinceCode].sum += calculateAverage(groupedData[key], 'retail_and_recreation_percent_change_from_baseline');
+                    ProvAve_retail[provinceCode].count += 1;
                 }
             }
 
-            // Calculate final ave for each province
+            // Calculate final average for each province
             var provinceLabels = [];
             var Prov_average = [];
+            var Prov_average_grocery = [];
+            var Prov_average_retail = []; // Added for retail_and_recreation_percent_change_from_baseline
 
             for (var provinceCode in ProvAve) {
                 if (ProvAve.hasOwnProperty(provinceCode)) {
                     var ave = ProvAve[provinceCode].sum / ProvAve[provinceCode].count;
-                    console.log(`Ave Workplaces % Change in ${provinceCode}:`, ave);
-
                     provinceLabels.push(provinceCode);
                     Prov_average.push(ave);
                 }
             }
-            // Render bar chart
-            // Render line chart for workplaces
-            AveProv_chart('chart1', provinceLabels, Prov_average, 'Workplaces Chart', 'Average Workplace Mobility Change');
 
-            // Calculate final ave for each province for groceries
-            var provinceLabels_grocery = [];
-            var Prov_average_grocery = [];
-            for (var provinceCode in ProvAve_grocery) {
+            for (var provinceCode in ProvAve_grocery) { //for each province code check the property and then
                 if (ProvAve_grocery.hasOwnProperty(provinceCode)) {
                     var aveGrocery = ProvAve_grocery[provinceCode].sum / ProvAve_grocery[provinceCode].count;
-                    console.log(`Ave Grocery % Change in ${provinceCode}:`, aveGrocery);
-    
-                    provinceLabels_grocery.push(provinceCode);
                     Prov_average_grocery.push(aveGrocery);
                 }
             }
-            AveProv_chart('chart2', provinceLabels_grocery, Prov_average_grocery, 'Grocery Chart', 'Average Grocery and Pharmacy Mobility Change');
 
+            for (var provinceCode in ProvAve_retail) { //for each province code check the property and then push to the array
+                if (ProvAve_retail.hasOwnProperty(provinceCode)) {
+                    var aveRetail = ProvAve_retail[provinceCode].sum / ProvAve_retail[provinceCode].count;
+                    Prov_average_retail.push(aveRetail);
+                }
+            }
+
+            // Render bar chart
+            AveProv_chart('chart1', provinceLabels, Prov_average, 'Workplaces mobility change % over 10 months', 'Average Workplace Mobility Change');
+            AveProv_chart_bar('chart2', provinceLabels, Prov_average_grocery, 'Grocery mobility change % over 10 months', 'Average Grocery and Pharmacy Mobility Change');
+            // Added for retail_and_recreation_percent_change_from_baseline
+            AveProv_pieChart('chart3', provinceLabels, Prov_average_retail, 'Retail mobility change % over 10 months', 'Average Retail and Recreation Mobility Change');
         });
 });
 
-function AveProv_chart(canvasId, labels, data, chartLabel, yAxisLabel) {
+function AveProv_chart(canvasId, labels, data, chartLabel, yAxisLabel) { // Added for retail_and_recreation_percent_change_from_baseline
+    var ctx = document.getElementById(canvasId).getContext('2d'); //get the canvas element then get the context
+    var myChart = new Chart(ctx, {
+        type: 'line', //chart type
+        data: {
+            labels: labels,
+            datasets: [{
+                label: chartLabel,
+                data: data,
+                backgroundColor: '#03fca9',
+                borderColor: '#004d33',
+                borderWidth: 2,
+                hoverBackgroundColor: '#03fca9',
+            }, {
+                type: 'line',
+                label: 'Average',
+                fill: false,
+                data: Array(data.length).fill(data.reduce((a, b) => a + b, 0) / data.length), //get an array to calculate the average
+                borderColor: 'rgba(127, 0, 0, 1)', //set the border colour
+                borderWidth: 1,
+                borderDash: [5, 5]
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    ticks: {
+                        beginAtZero: true,
+                        callback: function (value, index, values) {
+                            return value + ' %';
+                        }
+                    },
+                    title: {
+                        display: true,
+                        text: yAxisLabel //add the y axis title
+                    }
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Province by short code' //add the x axis title
+                    }
+                }
+            }
+        }
+    });
+}
+
+function AveProv_chart_bar(canvasId, labels, data, chartLabel, yAxisLabel) { //bar chart duplicate from the line chart
     var ctx = document.getElementById(canvasId).getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
@@ -180,3 +242,36 @@ function AveProv_chart(canvasId, labels, data, chartLabel, yAxisLabel) {
     });
 }
 
+function AveProv_pieChart(canvasId, labels, data, chartLabel) {
+    var ctx = document.getElementById(canvasId).getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: data,
+                backgroundColor: [
+                    '#c93c32',
+                    '#de8f3a',
+                    '#dede3a',
+                    '#6dde35',
+                    '#35debf',
+                    '#287aa6',
+                    '#2135a6',
+                    '#6126bf',
+                    '#b526bf',
+                    '#5c3e41',
+                ],
+                hoverOffset: 4
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: chartLabel
+                }
+            }
+        }
+    });
+}
